c20f56dc447a9619cb571b6bad95901dbf5592db
MolnarKristof0719 <csangcsungcsing@gmail.com>
2025-11-24 12:24:08 +0100

    Sport migracio kesz

--- server/app/Models/Student.php
+++ server/app/Models/Student.php
@@ -9,4 +9,30 @@ class Student extends Model
 {
     /** @use HasFactory<\Database\Factories\StudentFactory> */
     use HasFactory;
+
+    protected $fillable = [
+    'diakNev',
+    'schoolclassId',
+    'neme',
+    'iranyitoszam',
+    'lakHelyseg',
+    'lakCim',
+    'szulHelyseg',
+    'szulDatum',
+    'igazolvanyszam',
+    'atlag',
+    'osztondij',
+];
+
+    // diák -> osztály (több diák tartozik egy osztályhoz)
+    public function schoolclass()
+    {
+        return $this->belongsTo(Schoolclass::class, 'schoolclassId');
+    }
+
+    // diák -> sportok (N-N)
+    public function sports()
+    {
+        return $this->belongsToMany(Sport::class, 'playingsports', 'studentId', 'sportId');
+    }
 }
